72e72f7395d454d0152ba7a60d9753980b8c8d43
Sarthak <notsarthakshah@gmail.com>
2024-02-23 11:39:14 +0530

    auth implemented -ui

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBS7D92fRwIaUkQcVcb05Tp9fDRLSqQdbE",
+  authDomain: "apna-96cc0.firebaseapp.com",
+  projectId: "apna-96cc0",
+  storageBucket: "apna-96cc0.appspot.com",
+  messagingSenderId: "281143405167",
+  appId: "1:281143405167:web:385d6eb130fd8cb9af7b9a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
